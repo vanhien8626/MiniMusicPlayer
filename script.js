3312ac02116a268fae947a9e5f368e348ff8c7db
Nguyễn Văn Hiến <129392400+vanhien8626@users.noreply.github.com>
2025-09-18 17:30:23 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-/*
-design by Voicu Apostol.
-design: https://dribbble.com/shots/3533847-Mini-Music-Player
-I can't find any open music api or mp3 api so i have to download all musics as mp3 file.
-You can fork on github: https://github.com/muhammederdem/mini-player
-*/
 
 new Vue({
   el: "#app",
